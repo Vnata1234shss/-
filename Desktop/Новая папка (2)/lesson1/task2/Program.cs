cf3cc9d37adb7cf73df5e09aab8c5afcacc7bc72
Natalia Visloguzova <vnatalija61@gmail.com>
2024-01-25 17:38:31 +0300

    lesson 1: task2

---
+++ Desktop/Новая папка (2)/lesson1/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 6;
+int b = 3;
+int c = 3;
+int d = 4;
+int e = 1;
+
+int max = a;
+
+if(b>max)
+{
+    max = b;
+}
+
+if(c>max)
+{
+    max = c;
+}
+
+if(d>max)
+{
+    max = d;
+}
+
+if(e>max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
