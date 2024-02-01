5ee5f82e91a61c94f668ed3c99708f87c1836f08
Natalia Visloguzova <vnatalija61@gmail.com>
2024-02-01 18:28:06 +0300

    первый массив

---
+++ Desktop/с#/lesson4/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 7;
+arr[4] = 9;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+// System.Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
